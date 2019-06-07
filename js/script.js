f69c1d3b0b6aba0cfedff17b8b775f76c8cbae77
Glori <46545325+gtoro29@users.noreply.github.com>
2019-06-06 23:03:46 -0400

    Update script.js
    //added if statements
    //corrected dates
    // added html string requirements
    //revised code notes/comments

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 /* Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator, Submitted by G.Toro, 6/2019 */
 
-//This array has five objects, each object contains four properties: quote, source, year, and type.
-
+//This array contains the data of the quotes, sources, years if any, and quote type.
 var quotes = [{
         quote: "Run often.Run long.But never outrun your joy of running.",
         source: "Julie Isphording, former Olympic runner",
-        year: " ",
+        year: "1961-Present",
         type: " running, motivational"
     },
     {
@@ -18,48 +17,50 @@ var quotes = [{
     {
         quote: "A race is a work of art that people can look at and be affected in as many ways theyre capable of understanding.",
         source: " Steve Prefontaine, Holder of Seven World Records",
-        year: " ",
+        year: "1951-1975",
         type: "inspirational, sports"
     },
     {
         quote: "Don't dream of winning, train for it!",
         source: " Mo Farah, Olympic long-distance runner",
-        year: " ",
-        type: "running, motivational "
+        year: "1983-Present",
+        type: "running, motivational"
     },
     {
         quote: "When anyone tells me I can 't do anything, I'm just not listening anymore.",
         source: "Florence Griffith Joyner, Three-Time Gold Olympian",
-        year: " ",
-        type: "inspirational, determination"
+        year: " ", //1959-1998
+        type: " "
     }
 ];
-//This prints the quotes array into the console
-console.log(quotes);
 
-
-
-//Random quote generator function--uses the length of the array of quotes to generate a random quote, then it returns this random quote
+//Random quote generator function--using the length of the array of to generate a random quote, then returning the quote.
 function getRandomQuote() {
     var randomQuote = Math.floor(Math.random() * quotes.length);
-
     return quotes[randomQuote];
 }
 
-
-
-//this function prints the random quote onto the page
+//this function prints the quote onto the page
 function printQuote() {
     var currentQuote = getRandomQuote();
+    //this line declares a local variable: html
     var html = "<p class='quote'> " + currentQuote.quote + "</p>";
+    //this line adds the source property to the local html variable---adds to string
     html += "<p class='source'> " + currentQuote.source;
-
+    //this statement verifies if there is a type property and---adds it to the string
+    if ("type" in currentQuote) {
+        html += "<span class='type'>" + currentQuote.type + "</span>";
+    }
+    //this statement verifies if there is a year property and---adds it to the string
+    if ("year" in currentQuote) {
+        html += "<span class='year'>" + currentQuote.year + "</span>";
+    }
+    //this line of code completes the html string
     document.getElementById("quote-box").innerHTML = html;
+    //this line completes the clickable button
     document.getElementById("loadQuote");
-}
-
+};
 //this loads a randomly generated quote each time the button is clicked---no changes made to the code below, per instructions. 
 {
     document.getElementById('loadQuote').addEventListener("click", printQuote);
-
 };
